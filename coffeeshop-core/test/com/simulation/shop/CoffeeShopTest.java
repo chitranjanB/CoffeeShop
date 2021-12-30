2d723ea7e7e3ad448262dae7ce1cfe87826a086b
Chitranjan Babu <chitranjan.pc8@gmail.com>
2021-12-30 20:13:19 +0530

    coffeeshop-core - completablefuture_multiinstance_fileio_polling - Updated tests

--- coffeeshop-core/test/com/simulation/shop/CoffeeShopTest.java
+++ coffeeshop-core/test/com/simulation/shop/CoffeeShopTest.java
@@ -2,6 +2,7 @@ package com.simulation.shop;
 
 import java.util.concurrent.ExecutionException;
 
+import com.simulation.shop.config.Config;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -18,11 +19,10 @@ public class CoffeeShopTest {
 
 	@Test
 	public void test_10_threads() throws Exception {
-		int limit = 10;
-		CoffeeUtility.loadupBeans(limit,limit);
-		CoffeeUtility.loadupMilk(limit,limit);
-		CoffeeShop shop = new CoffeeShop();
 		int customers = 10;
+		CoffeeUtility.loadupBeans(CoffeeUtility.fetchRequiredMachines(customers), Config.BEANS_INVENTORY_LIMIT);
+		CoffeeUtility.loadupMilk(CoffeeUtility.fetchRequiredMachines(customers), Config.MILK_INVENTORY_LIMIT);
+		CoffeeShop shop = new CoffeeShop();
 		shop.start(customers);
 	}
 }
