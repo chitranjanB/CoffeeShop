1566d6f26aa9aba0fd7967fdec2f582234ebb90d
Chitranjan Babu <chitranjan.pc8@gmail.com>
2022-10-13 08:28:18 +0530

    coffeeshop-node-proxy - spring-rest-master - Implemented code refactoring for proxy

--- coffeeshop-node-proxy/index.js
+++ coffeeshop-node-proxy/index.js
@@ -5,46 +5,27 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 app.use(morgan("dev"));
 
-const PORT = 80;
-const HOST = "localhost";
+const PROXY_HOST = "localhost";
+const PROXY_PORT = 80;
 
 const HELLO_MS = "http://localhost:3032/hello";
 const CODIFY_MS = "http://localhost:3031/codify";
 const LINKER_MS = "http://localhost:3033/linker";
 
-app.use(
-  "/hello",
-  createProxyMiddleware({
-    target: HELLO_MS,
-    changeOrigin: true,
-    pathRewrite: {
-      [`^/hello`]: "",
-    },
-  })
-);
-
-app.use(
-  "/codify",
-  createProxyMiddleware({
-    target: CODIFY_MS,
-    changeOrigin: true,
-    pathRewrite: {
-      [`^/codify`]: "",
-    },
-  })
-);
-
-app.use(
-  "/linker",
-  createProxyMiddleware({
-    target: LINKER_MS,
-    changeOrigin: true,
-    pathRewrite: {
-      [`^/linker`]: "",
-    },
-  })
-);
+[HELLO_MS, CODIFY_MS, LINKER_MS].forEach((target) => {
+  const path = target.substring(target.lastIndexOf("/"));
+  app.use(
+    path,
+    createProxyMiddleware({
+      target: target,
+      changeOrigin: true,
+      pathRewrite: {
+        [`^${path}`]: "",
+      },
+    })
+  );
+});
 
 app.listen(PORT, HOST, () => {
-  console.log(`Coffeeshop Node Proxy Running at ${HOST}:${PORT}`);
+  console.log(`coffeeshop-node-proxy running at ${PROXY_HOST}:${PROXY_PORT}`);
 });
